578cf186f1c30e002c538cf04f5d45932f15016b
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-07-29 15:38:48 +0200

    Handle errors
    in case te data couldn't be fetched from db successfully

--- food_order/src/components/Meals/AvailableMeals.js
+++ food_order/src/components/Meals/AvailableMeals.js
@@ -16,10 +16,16 @@ const AvailableMeals = () => {
 
   const [meals, setMeals] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData= async() => {
       const response = await fetch('https://tasks-52028-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+      
+      if(!response.ok) {
+        throw new Error('Something went wrong');
+      }
+     
       const data = await response.json();
 
       const mealDB = [];
@@ -34,18 +40,28 @@ const AvailableMeals = () => {
       setMeals(mealDB);
     };
 
-    fetchData();
+    fetchData().catch((error) => {
+      setError(error.message);
+    });
     setLoading(false);
-    
+  
   }, []);
 
 
   if (loading) {
     return(
       <section className={styles.loading}>
-        Loading...
+        <p>Loading...</p>
       </section>
     );
+  }  
+  
+  if(error) {
+    return(
+      <section className={styles.error}>
+        <p>{error}</p>
+      </section>
+    )
   }
 
 
