bb320b320b137e413ee532ca42d7755a1833032a
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-08-10 15:01:32 +0200

    handling states using nested routes

--- router-advance/src/pages/QuoteDetail.js
+++ router-advance/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 
 
 import Comments from "../components/comments/Comments";
@@ -42,6 +42,13 @@ const QuoteDetail = () => {
     return(
         <Fragment>
             <HighLightedQuote text={quote.content} author={quote.author} />
+            <Route path={`/quotes/${params.quoteId}`} exact>
+                <div className="centered">
+                    <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+                        Comments
+                    </Link>
+                </div>
+            </Route>
             <Route path={`/quotes/${params.quoteId}/comments`}>
                 <Comments />
             </Route>
