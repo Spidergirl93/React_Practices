a89803eac284483f360d0e69a15e8a91f28de65e
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-08-10 12:15:42 +0200

    programatic navigation with reacy router

--- router-advance/src/pages/NewQuote.js
+++ router-advance/src/pages/NewQuote.js
@@ -1,10 +1,17 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 
 const NewQuote = () => {
 
+
+    const history = useHistory();
+
     const addQuoteHandler = (newQuote) => {
         console.log(newQuote);
+
+        history.push('/quotes');
     };
 
 
---
+++ router-advance/src/pages/NotFound.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return(
+        <div className="centered">
+            <p>404! The page is not found.</p>
+        </div>
+    );
+};
+
+export default NotFound;
