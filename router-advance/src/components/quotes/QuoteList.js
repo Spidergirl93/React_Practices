949f776de4feb64053f6da3f70e29470329e0ac0
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-08-10 14:35:42 +0200

    sort with param queries

--- router-advance/src/components/quotes/QuoteList.js
+++ router-advance/src/components/quotes/QuoteList.js
@@ -5,27 +5,46 @@ import { useHistory, useLocation } from 'react-router-dom';
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
+
+
 const QuoteList = (props) => {
 
   const history = useHistory();
 
   const location = useLocation();
 
-  console.log(location);
+  const queryParams = new URLSearchParams(location.search);
+
+  const isAsc = queryParams.get('sort') === 'asc';
   
+  const sortedQuotes = sortQuotes(props.quotes, isAsc);
 
   const sortHandler = () => {
 
-    history.push('/quotes?sort=asc');
+    history.push('/quotes?sort=' + (isAsc ? 'desc' : 'asc'));
+
   };
+
+
 
   return (
     <Fragment>
       <div className={classes.sorting}>
-        <button onClick={sortHandler}>Sort Ascending</button>
+        <button onClick={sortHandler}>Sort {isAsc ? 'Descending' : 'Ascending'}</button>
       </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
