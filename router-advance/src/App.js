26e6920d383ffd7083e680bc5f11c34b68d6c26e
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-08-11 23:31:28 +0200

    use lazy loading to optimize

--- router-advance/src/App.js
+++ router-advance/src/App.js
@@ -1,32 +1,40 @@
+import React, { Suspense} from 'react'; 
 import { Route, Switch, Redirect} from 'react-router-dom';
 
-import NewQuote from './pages/NewQuote';
-import QuoteDetail from './pages/QuoteDetail';
-import AllQuote from './pages/AllQuotes';
-import Layout from './components/layout/Layout';
-import NotFound from './pages/NotFound';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const AllQuote = React.lazy(() => import('./pages/AllQuotes'));
+const Layout = React.lazy(() => import('./components/layout/Layout'))
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes'/>
-        </Route>
-        <Route path='/quotes' exact>
-          <AllQuote />
-        </Route>
-        <Route path='/quotes/:quoteId'>
-          <QuoteDetail />
-        </Route>
-        <Route path='/new-quote'>
-          <NewQuote />
-        </Route>
-        <Route path='*'>
-          <NotFound />
-        </Route>
-      </Switch>
-
+      <Suspense fallback={
+        <div className='centered'>
+          <LoadingSpinner />
+        </div>
+      }>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/quotes'/>
+          </Route>
+          <Route path='/quotes' exact>
+            <AllQuote />
+          </Route>
+          <Route path='/quotes/:quoteId'>
+            <QuoteDetail />
+          </Route>
+          <Route path='/new-quote'>
+            <NewQuote />
+          </Route>
+          <Route path='*'>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
