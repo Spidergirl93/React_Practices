30a1ae15682284ef7442daf20f1f13c8304973a2
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-08-11 12:59:37 +0200

    show new comments without any reload

--- router-advance/src/components/comments/Comments.js
+++ router-advance/src/components/comments/Comments.js
@@ -1,14 +1,48 @@
-import { useState } from 'react';
-
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import CommentsList from './CommentsList';
+import LoadingSpinner from '../UI/LoadingSpinner';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const {sendRequest, status, data, error} = useHttp(getAllComments, true);
+  const params = useParams();
+
+  const { quoteId } = params;
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest,quoteId]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
+
+  const finishAddCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+    setIsAddingComment(false);
+  }, [sendRequest, quoteId, setIsAddingComment]);
+
+  const comments = () => {
+    if (status === 'pending') {
+      return <div className='centered'>
+        <LoadingSpinner />;
+      </div>;
+
+    } else if (status === 'completed' && (data.length === 0 || !data)) {
+      return <p className='centered'>No comments were found</p>;
+
+    } else if (error) {
+      return <p className='centered'>An error occured while loading the comments</p>;
+
+    } else {
+      return <CommentsList comments={data} />;
+    }
+  }
   
   return (
     <section className={classes.comments}>
@@ -18,8 +52,8 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && <NewCommentForm onAddComment={finishAddCommentHandler} />}
+      {comments()}
     </section>
   );
 };
--- router-advance/src/components/comments/NewCommentForm.js
+++ router-advance/src/components/comments/NewCommentForm.js
@@ -1,20 +1,41 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
+import { useParams } from 'react-router-dom';
+
+import useHttp from '../../hooks/use-http';
+
+import { addComment } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './NewCommentForm.module.css';
 
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
+  const {sendRequest, status, error} = useHttp(addComment);
+  const params = useParams();
+
+  const {onAddComment} = props;
 
   const submitFormHandler = (event) => {
     event.preventDefault();
-
-    // optional: Could validate here
-
-    // send comment to server
+    const comment = commentTextRef.current.value;
+    sendRequest(
+      {
+        quoteId: params.quoteId, 
+        commentData: {text: comment}
+      }
+      );
   };
+
+  useEffect(() => {
+    if(!error && status === 'completed') {
+      onAddComment();
+    }
+  }, [onAddComment, status, error]);
+
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === 'pending' && <LoadingSpinner />}
       <div className={classes.control} onSubmit={submitFormHandler}>
         <label htmlFor='comment'>Your Comment</label>
         <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
