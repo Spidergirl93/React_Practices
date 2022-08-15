cc2c5860f9350a91d0f2223d0126665d4bd0c38c
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-08-15 10:27:33 +0200

    first commit

--- Authentication/src/components/Auth/AuthForm.js
+++ Authentication/src/components/Auth/AuthForm.js
@@ -1,12 +1,10 @@
-import { useState, useRef, useContext } from 'react';
-import { useHistory } from 'react-router-dom';
-import classes from './AuthForm.module.css';
-import AuthContext from '../../store/auth-context';
-
+import { useState, useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import classes from "./AuthForm.module.css";
+import AuthContext from "../../store/auth-context";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
-
   const emailRef = useRef();
   const passRef = useRef();
 
@@ -25,69 +23,70 @@ const AuthForm = () => {
     const enteredPass = passRef.current.value;
     let url;
 
-    if(isLogin) {
-      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDM9-YuI0iY7iX00-qZO8-w37Rma18MjRQ';
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDM9-YuI0iY7iX00-qZO8-w37Rma18MjRQ";
     } else {
-      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDM9-YuI0iY7iX00-qZO8-w37Rma18MjRQ'
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDM9-YuI0iY7iX00-qZO8-w37Rma18MjRQ";
     }
 
-    fetch(
-      url,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: enteredEmail,
-          password: enteredPass,
-          returnSecureToken: true
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then((res) => {
-        if(res.ok) {
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPass,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
           return res.json();
         } else {
           return res.json().then((data) => {
             console.log(data);
-            let errorMsg = 'Failed';
+            let errorMsg = "Failed";
 
-            if(data && data.error && data.error.message) {
+            if (data && data.error && data.error.message) {
               errorMsg = data.error.message;
             }
 
             throw new Error(errorMsg);
-            
-          })
-        } 
-      }
-    ).then((data) => {
-      loginContext.login(data.idToken);
-      history.replace('/');
-    }).catch (error => {
-      alert(error.message);
-    });
+          });
+        }
+      })
+      .then((data) => {
+        loginContext.login(data.idToken);
+        history.replace("/");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required  ref={emailRef} />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required ref={passRef} />
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" required ref={passRef} />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
---
+++ Nextjs/pages/News.js
@@ -0,0 +1,5 @@
+const News = () => {
+  return <h1>The news Page</h1>;
+};
+
+export default News;
