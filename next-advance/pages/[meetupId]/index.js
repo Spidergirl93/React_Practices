99dffdaa448312691238286d306d02fe1eed0117
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-08-15 21:39:47 +0200

    go to the correspondig page of each individual meetup

--- next-advance/pages/[meetupId]/index.js
+++ next-advance/pages/[meetupId]/index.js
@@ -1,74 +1,63 @@
-import { useRouter } from "next/router";
+import { MongoClient, ObjectId } from "mongodb";
 
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const DUMMY_DATA = [
-  {
-    id: "1",
-    title: "Good Place",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-    address: "Some street",
-    description: "This place is amazing! You have to visit here some time",
-  },
-  {
-    id: "2",
-    title: "Better Place",
-    image:
-      "https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1275&q=80",
-    address: "Neverland",
-    description: "This place is amazing! You have to visit here some time",
-  },
-  {
-    id: "3",
-    title: "Best Place",
-    image:
-      "https://images.unsplash.com/photo-1520637102912-2df6bb2aec6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
-    address: "Mordor",
-    description: "This place is amazing! You have to visit here some time",
-  },
-  {
-    id: "4",
-    title: "Awesome Place",
-    image:
-      "https://images.unsplash.com/photo-1562832135-14a35d25edef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2145&q=80",
-    address: "Doesn't exist!",
-    description: "This place is amazing! You have to visit here some time",
-  },
-];
-
-const MeetupDetailPage = () => {
-  const router = useRouter();
-
-  const meetupId = router.query.meetupId;
-
-  const item = DUMMY_DATA.find((meetUp) => {
-    return meetUp.id === meetupId;
-  });
-
+const MeetupDetailPage = (props) => {
   return (
     <MeetupDetail
-      id={item.id}
-      title={item.title}
-      address={item.address}
-      image={item.image}
-      description={item.description}
+      id={props.meetupData._id}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      image={props.meetupData.image}
+      description={props.meetupData.description}
     />
   );
 };
 
-export const getStaticProps = async (context) => {
+export const getStaticPaths = async () => {
+  const client = await MongoClient.connect(
+    "mongodb+srv://walkmary:SesameOpen@meetup.ltcqybe.mongodb.net/?retryWrites=true&w=majority"
+  );
 
+  const db = client.db();
+
+  const meetupCollection = db.collection("meetups");
+
+  const meetups = await meetupCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
+  return {
+    fallback: false,
+    paths: meetups.map((meetup) => ({
+      params: {
+        meetupId: meetup._id.toString(),
+      },
+    })),
+  };
+};
+
+export const getStaticProps = async (context) => {
   const meetupId = context.params.meetupId;
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://walkmary:SesameOpen@meetup.ltcqybe.mongodb.net/?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupCollection = db.collection("meetups");
+
+  const foundMeetup = await meetupCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
 
   return {
     props: {
-      id: meetupId,
-      title: "Good Place",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-      address: "Some street",
-      description: "This place is amazing! You have to visit here some time",
+      meetupData: {
+        ...foundMeetup,
+        _id: foundMeetup._id.toString(),
+      },
     },
   };
 };
