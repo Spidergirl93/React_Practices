b7aea712b1de47addbc4ad71dc2d20036979e23d
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-08-17 12:05:33 +0200

    replace redux with context hook

--- hooks_insteadOf_redux/src/index.js
+++ hooks_insteadOf_redux/src/index.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
+import ReactDOM from 'react-dom/client';
+
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
+import ProductsProvider from './contexts/products-context';
 
-const store = createStore(rootReducer);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(
-  <Provider store={store}>
+root.render(
+  <ProductsProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </ProductsProvider>,
 );
